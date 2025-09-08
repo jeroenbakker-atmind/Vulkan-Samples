c1cb977feb7769f39b560d4bd41e9107f73a4e43
Jeroen Bakker <jeroen@blender.org>
2025-09-08 17:16:48 +0200

    Initial commit

---
+++ samples/api/wide_gamut/wide_gamut.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2019-2024, Arm Limited and Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "rendering/render_pipeline.h"
+#include "scene_graph/components/camera.h"
+#include "vulkan_sample.h"
+
+class WideGamut : public vkb::VulkanSampleC
+{
+  public:
+	WideGamut();
+
+	virtual bool prepare(const vkb::ApplicationOptions &options) override;
+
+	virtual ~WideGamut() = default;
+};
+
+std::unique_ptr<vkb::VulkanSampleC> create_wide_gamut();
