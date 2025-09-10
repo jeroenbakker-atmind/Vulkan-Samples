2801d628ec36dedbe7f3879c0037a9c1398f6e52
Jeroen Bakker <jeroen@blender.org>
2025-09-10 12:33:47 +0200

    try to render ui

--- samples/api/wide_gamut/wide_gamut.cpp
+++ samples/api/wide_gamut/wide_gamut.cpp
@@ -27,6 +27,8 @@
 
 WideGamut::WideGamut()
 {
+	/* Enable VK_EXT_swapchain_color_space_ext when available. Without this extension swapchains should not support additional color spaces. */
+	add_instance_extension(VK_EXT_SWAPCHAIN_COLOR_SPACE_EXTENSION_NAME, true);
 }
 
 bool WideGamut::prepare(const vkb::ApplicationOptions &options)
@@ -56,6 +58,43 @@ bool WideGamut::prepare(const vkb::ApplicationOptions &options)
 	create_gui(*window, &get_stats());
 
 	return true;
+}
+
+void WideGamut::build_command_buffers()
+{
+}
+
+void WideGamut::render(float /*delta_time*/)
+{
+	if (!prepared)
+	{
+		return;
+	}
+	//draw();
+}
+
+/**
+ * 	@brief Setting custom surface format priority list to required VK_FORMAT_B8G8R8A8_UNORM format
+ */
+void WideGamut::create_render_context()
+{
+	auto surface_priority_list = std::vector<VkSurfaceFormatKHR>{
+	    surface.formats[surface.format_index],
+	    /* Fallback to sRGB. */
+	    {VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR},
+	};
+	VulkanSample::create_render_context(surface_priority_list);
+}
+
+void WideGamut::on_update_ui_overlay(vkb::Drawer &drawer)
+{
+	if (drawer.header("Settings"))
+	{
+		if (drawer.combo_box("Surface Color Space", &surface.format_index, surface.format_names))
+		{
+			// recreate_swapchain();
+		}
+	}
 }
 
 std::unique_ptr<vkb::VulkanSampleC> create_wide_gamut()
--- samples/api/wide_gamut/wide_gamut.h
+++ samples/api/wide_gamut/wide_gamut.h
@@ -17,16 +17,33 @@
 
 #pragma once
 
+#include "api_vulkan_sample.h"
 #include "rendering/render_pipeline.h"
 #include "scene_graph/components/camera.h"
-#include "vulkan_sample.h"
 
-class WideGamut : public vkb::VulkanSampleC
+class WideGamut : public ApiVulkanSample
 {
+	struct
+	{
+		const std::vector<VkSurfaceFormatKHR> formats = {
+		    {VK_FORMAT_R16G16B16A16_SFLOAT, VK_COLOR_SPACE_EXTENDED_SRGB_LINEAR_EXT},
+		    {VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR},
+		};
+		const std::vector<std::string> format_names = {
+		    std::string("Extended sRGB (Linear)"),
+		    std::string("sRGB"),
+		};
+		int format_index = 0;
+	} surface;
+
   public:
 	WideGamut();
 
 	virtual bool prepare(const vkb::ApplicationOptions &options) override;
+	virtual void create_render_context() override;
+	virtual void build_command_buffers() override;
+	virtual void render(float delta_time) override;
+	virtual void on_update_ui_overlay(vkb::Drawer &drawer) override;
 
 	virtual ~WideGamut() = default;
 };
